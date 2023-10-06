248d2d794ecb72ff937533bd7da3dec33979bd6d
asdfw34f <daniil2002.d11@gmail.com>
2023-10-06 20:10:58 +0300

    dagger done

--- src/main/kotlin/Five/News/DaggerComponent.kt
+++ src/main/kotlin/Five/News/DaggerComponent.kt
@@ -1,14 +1,16 @@
 package Five.News
 
 import Five.News.Helper.*
+import Five.News.Services.ManipulationService
 import dagger.Component
 
 @Component
 interface DaggerComponent {
-    fun getAdder(): RateAdder
-    fun getEditor(): RateEditor
-    fun getRemover(): RateRemover
-    fun getSearcher(): RateSearcher
-    fun getPrinter(): RatesPrinter
-    fun getSorter(): RatesSorter
+//    fun getAdder(): RateAdder
+//    fun getEditor(): RateEditor
+//    fun getRemover(): RateRemover
+//    fun getSearcher(): RateSearcher
+//    fun getPrinter(): RatesPrinter
+//    fun getSorter(): RatesSorter
+    fun getManipulationService():ManipulationService
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,13 +2,7 @@ import Five.News.DaggerDaggerComponent
 import Five.News.Services.ManipulationService
 
 fun main(args: Array<String>) {
-    val d = DaggerDaggerComponent.create()
-    var ms = ManipulationService(
-        d.getEditor(),
-        d.getAdder(),
-        d.getRemover(),
-        d.getSorter(),
-        d.getSearcher(),
-        d.getPrinter()
-    )
+    val d = DaggerDaggerComponent.builder().build()
+    val manipulationService = d.getManipulationService()
+
 }
